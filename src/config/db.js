3a5b1f0bd38c8072ccaa519f689fdde566c93fb9
Alhadji Oumate <alhadjioum0@gmail.com>
2025-03-05 23:22:54 +0100

    feat: created user schema

--- src/config/db.js
+++ src/config/db.js
@@ -1,42 +1,20 @@
-import { MongoClient, ServerApiVersion } from 'mongodb';
-import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
-dotenv.config();
+// Connect to MongoDB
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.DATABASE_URL, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    });
 
-const uri = process.env.URL
-  ? process.env.DATABASE_URL
-  : 'mongodb://localhost:27017';
-
-class DBClient {
-  constructor() {
-    this.db = null;
-    this.connect();
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
   }
+};
 
-  async connect() {
-    try {
-      const connectTimeoutMS = 60000;
-      const client = new MongoClient(uri, {
-        serverApi: {
-          version: ServerApiVersion.v1,
-          strict: true,
-          deprecationErrors: true,
-        },
-        connectTimeoutMS,
-      });
-      await client.connect();
-      this.db = client.db('busBookingDB');
-      console.log('Connected to database!!!');
-    } catch (error) {
-      console.error('Could not connect to database\n', error);
-    }
-  }
-
-  isAlive() {
-    return !!this.db;
-  }
-}
-
-const dbClient = new DBClient();
-
-export default dbClient;
+// Export the function
+export default connectDB;
